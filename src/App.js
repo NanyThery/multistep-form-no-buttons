f3abec5ae6d79976866ebec72a5d9328080f2789
Nany Thery <nadinethery@gmail.com>
2021-02-17 12:15:30 +0100

    feat: main structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import FormContainer from "./containers/FormContainer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is a multi-step form demo</h1>
+      <h2>But with no buttons...</h2>
+      <FormContainer />
     </div>
   );
 }
