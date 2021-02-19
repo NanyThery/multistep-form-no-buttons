1fba3928c794dd0fe519842eff7e59276023ebe8
Nany Thery <nadinethery@gmail.com>
2021-02-19 17:59:05 +0100

    feat: step/1

---
+++ src/components/Step0.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledContainer = styled.div``;
+
+const Step0 = () => {
+  return (
+    <StyledContainer>
+      <p>This is step 0</p>
+    </StyledContainer>
+  );
+};
+
+export default Step0;
